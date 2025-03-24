50193c74198eada6dd20a5c3573e55c8b8261212
Juan Felipe Parra Runceria <85968522+juanfel254@users.noreply.github.com>
2025-03-23 23:14:18 -0500

    Revert "Feature/i18n integration (#1)" (#2)
    This reverts commit e413e003772c4b0c48d03a1b5d7973f024434548.

--- src/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './useComponentTranslations';
-export * from './useLocale';
-export * from './useGlobalTranslations'; 
--- src/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './getServerTranslations';
-export * from './getGlobalTranslations'; 
